1b4daec6af4e1b23a014298312bef65fd9c319a9
Yatharth Champaneria <ychampaneria3@gmail.com>
2021-07-11 01:39:01 +0530

    Dynamic Form

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,137 +1,129 @@
-import React, { Component } from 'react'
+import React,  {useState} from 'react'
 import './Reports.css';
-class Users extends Component{
+import { v4 as uuidv4 } from 'uuid';
+// class Users extends Component{
     // counter= false;
-    constructor(props){
-     super(props);
+//     constructor(props){
+//      super(props);
     
-     this.state = {
-        counter: false    
-    }
+//      this.state = {
+//         counter: false    
+//     }
 
-    }
-    handlechangeall = (event) =>{
-     this.setState ( { [event.target.name] :event.target.value  })
-    }
-    
-    //     handlesubmit = (event) => {
-    //         if (this.state.auditTopic && this.state.auditTopicBrokers && this.state.auditSchema && this.state.consumerProperties
-    //             && this.state.auditDetails && this.state.rulebookName && this.state.rulebookJobTypes && this.state.rulebookType && this.state.ruleBookDetails
-    //             && this.state.tenant && this.state.topic && this.state.topicBrokers && this.state.ruleBookLogic && this.state.enable)
-    //  {
-    //      alert ("Your form has been submitted.");
-    //  // alert( JSON.stringify(this.state));
-    //  console.log( JSON.stringify(this.state));
-    //  event.preventDefault();
-    //  }
-    //  else
-    //     alert("One or more fields are incomplete");
-    
-    // }
-    updateCategory = (event) => {
-        if(event.target.checked) {
-           this.setState({ enable: event.target.value})
+//     }
+//     handlechangeall = (event) =>{
+//      this.setState ( { [event.target.name] :event.target.value  })
+//     }
+//     render(){
+//      return(
+//       <div>
+//       <br/>
+//         <span className="headings">User Info</span> <br/><br/>
+//         <form onSubmit = {this.handlesubmit} >
+        // <table className="yatharth">
+        // <tbody>
+        // <tr>
+        // <td><label>User ID: </label></td>
+        // <td><input  type="text" name="id"  value={this.state.id}  
+        //         onChange={this.handlechangeall} /> </td>
+        // </tr>
+        // <tr>
+        // <td><label> Version: </label></td>
+        // <td><input  type="text" name="version" value= {this.state.version} 
+        //         onChange={this.handlechangeall} /> </td>
+        //  </tr>
+        // <tr>
+        // <td><label> Data Type: </label></td>
+        // <td><input  type="text" name="dataType" value= {this.state.dataType} 
+        //         onChange={this.handlechangeall} /> </td>
+        // </tr>
+//         <button onClick={()=> {
+//             <tr>
+//                 <td><label> Rulebook Name </label></td>
+//                 <td><input  type="text" name="rulebookName"  value={this.state.rulebookName}  
+//                  onChange={this.handlechangeall} /></td>
+//             </tr>
+//             console.log("pants")
+//             }}>+ New User</button><br></br>
+//         </tbody>
+//         </table>   
+//         <input type="submit" value="Send" />
+//        </form>
+//       </div>
+//      )
+//     }
+
+function Users() {
+        const [inputFields, setInputFields] = useState([
+          { id: uuidv4(), userid: '', version: '', datatype: '' },
+        ]);
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+          console.log("InputFields", inputFields);
+        };
+      
+        const handleChangeInput = (id, event) => {
+          const newInputFields = inputFields.map(i => {
+            if(id === i.id) {
+              i[event.target.name] = event.target.value
+            }
+            return i;
+          })
+          
+          setInputFields(newInputFields);
         }
-    }
-    updateCategorys = (event) => {
-        if(event.target.checked) {
-           this.setState({ enableBigBen: event.target.value})
+      
+        const handleAddFields = () => {
+          setInputFields([...inputFields, { id: uuidv4(),  userid: '', version: '', datatype: '' }])
         }
-    }
-    userx = (event) => {
-        this.setState({counter: true})
-    }
-    render(){
-     return(
-      <div>
-      <br/>
-        <span className="headings">User Info</span> <br/><br/>
-        <form onSubmit = {this.handlesubmit} >
-        <table className="yatharth">
-        <tbody>
-        <tr>
-        <td><label>User ID: </label></td>
-        <td><input  type="text" name="id"  value={this.state.id}  
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>
-        <td><label> Version: </label></td>
-        <td><input  type="text" name="version" value= {this.state.version} 
-                onChange={this.handlechangeall} /> </td>
-         </tr>
-        <tr>
-        <td><label> Data Type: </label></td>
-        <td><input  type="text" name="dataType" value= {this.state.dataType} 
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <button onClick={()=> {
-            <tr>
-                <td><label> Rulebook Name </label></td>
-                <td><input  type="text" name="rulebookName"  value={this.state.rulebookName}  
-                 onChange={this.handlechangeall} /></td>
-            </tr>
-            console.log("pants")
-            }}>+ New User</button><br></br>
-        
-             
-        {/* <tr>
-        <td><label> Rulebook Job Types    </label></td>
-        <td><input  type="text" name="rulebookJobTypes" value= {this.state.rulebookJobTypes} 
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>
-        <td><label> Rulebook Type </label></td>
-        <td><input  type="text" name="rulebookType" value= {this.state.rulebookType} 
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>
-        <td><label> Enable </label></td>
-        <td><label><input  type="radio" name="enable"  value="false" //checked={this.props.checked === "false"}
-                onChange={this.updateCategory} />False    <input  type="radio" name="enable"  value="true" //checked={this.props.checked === "true"}
-                onChange={this.updateCategory} />True</label> </td>
-        </tr>
-        <tr>
-        <td><label> Rulebook Details </label></td>
-        <td><input  type="text" name="ruleBookDetails"  value={this.state.ruleBookDetails}  
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        {/* </tbody></table> */}
-        {/* <hr/> */}
-        {/* <span className="headings">Replay Service</span> <br/><br/> */}
-        {/* <table className="yatharth">
-        <tbody> */}
-        {/* <tr>
-        <td><label> Tenant </label></td>
-        <td><input  type="text" name="tenant"  value={this.state.tenant}  
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>   
-        <td><label> Topic </label></td>
-        <td><input  type="text" name="topic" value= {this.state.topic} 
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>
-        <td><label> Topic Brokers </label></td>
-        <td><input  type="text" name="topicBrokers" value= {this.state.topicBrokers} 
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>
-        <td><label> Rulebook Logic </label></td>
-        <td><input  type="text" name="ruleBookLogic"  value={this.state.ruleBookLogic}  
-                onChange={this.handlechangeall} /> </td>
-        </tr>
-        <tr>
-        <td><label> Enable Big Ben </label></td>
-        <td><label><input  type="radio" name="enableBigBen"  value="false" //checked={this.props.checked === "false"}
-                onChange={this.updateCategorys} />False    <input type="radio" name="enableBigBen"  value="true" //checked={this.props.checked === "true"}
-                onChange={this.updateCategorys} />True</label> </td>
-        </tr> */} 
-        </tbody>
-        </table>   
-        <input type="submit" value="Send" />
-       </form>
-      </div>
-     )
-    }
-}
-export default Users;
+        return (
+          <div>
+            <form onSubmit={handleSubmit}>
+            <table className="yatharth">
+            <tbody>
+              { inputFields.map(inputField => (
+                <div key={inputField.id}>
+                <tr>
+                <td><label>User ID: </label></td>
+                <td>
+                  <input 
+                    type ="text"
+                    name="userid"
+                    variant="filled"
+                    value={inputField.userid}
+                    onChange={event => handleChangeInput(inputField.id, event)}
+                  /></td></tr>
+                  <tr>
+                <td><label>Version: </label></td>
+                <td>
+                  <input 
+                    type ="text"
+                    name="version"
+                    variant="filled"
+                    value={inputField.version}
+                    onChange={event => handleChangeInput(inputField.id, event)}
+                  /></td></tr>
+                  <tr>
+                <td><label>Data type: </label></td>
+                <td>
+                  <input 
+                    type ="text"
+                    name="datatype"
+                    variant="filled"
+                    value={inputField.datatype}
+                    onChange={event => handleChangeInput(inputField.id, event)}
+                  /></td></tr>
+                  <button onClick={handleAddFields} >
+                    + Add User
+                  </button>
+                </div>
+              )) }
+              <button onClick={handleSubmit} className="send">submit</button>
+              </tbody>
+              </table>
+            </form>
+          </div>
+        );
+      }
+      export default Users;
